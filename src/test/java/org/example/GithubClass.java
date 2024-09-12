91599de5c15935f70a60f81fcddf917ef840a6ba
Amarnath8124 <amarnathamar8124@gmail.com>
2024-09-12 22:49:20 +0530

    V3 methods add

--- src/test/java/org/example/GithubClass.java
+++ src/test/java/org/example/GithubClass.java
@@ -12,5 +12,11 @@ public class GithubClass {
 	private void m3() {
 		System.out.println("3");
 	}
+	private void tc1() {
+		System.out.println("tc 1");
+	}
+	private void tc2() {
+		System.out.println("tc 2");
+	}
 
 }
