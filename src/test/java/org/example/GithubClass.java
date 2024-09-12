6612ea442e04e1cc75ffc196516370fc63cedcfd
Amarnath8124 <amarnathamar8124@gmail.com>
2024-09-12 21:35:08 +0530

    New methods Added

---
+++ src/test/java/org/example/GithubClass.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class GithubClass {
+	
+	private void m1() {
+		System.out.println("1");
+		
+	}
+	private void m2() {
+		System.out.println("2");
+	}
+	private void m3() {
+		System.out.println("3");
+	}
+
+}
